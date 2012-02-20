19262e8dd46644a0396221abc4f9b2c14143b0a7
sorenbs <sorenbs@gmail.com>
2012-02-20 01:54:41 +0100

    fix bug where it didn't stop if it didn't land on the exact arget. add NewDirection event when stopping. make it possible to set speed

--- moveto.js
+++ moveto.js
@@ -1,78 +1,74 @@
-﻿///1.8
-Crafty.c("MoveTo", {
-    _speed: 2,
+﻿Crafty.c("MoveTo", {
+	_speed: 2,
 
-    _onmousedown:function (e) {
-      this._target = { x: e.realX, y: e.realY };
-      var dx = e.realX - this.x, dy = e.realY - this.y;
-      this._movement={
-        x:(dx * this._speed) / (Math.sqrt(dx * dx + dy * dy)),
-        y:(dy * this._speed) / (Math.sqrt(dx * dx + dy * dy))
-      },
+	_onmousedown: function (e) {
+		this._target = { x: e.realX, y: e.realY };
+		var dx = e.realX - this.x, dy = e.realY - this.y;
+		this._movement = {
+			x: (dx * this._speed) / (Math.sqrt(dx * dx + dy * dy)),
+			y: (dy * this._speed) / (Math.sqrt(dx * dx + dy * dy))
+		},
       this.trigger('NewDirection', this._movement);
-    },
+	},
 
-    init: function () {
-      this.requires("Mouse");
+	moveTo: function(speed) {
+		this._speed = speed;
+		return this;
+	},
 
-      Crafty.addEvent(this, Crafty.stage.elem, "mousedown", this._onmousedown);
+	init: function () {
+		this.requires("Mouse");
 
-      /*
-       this.bind('NewDirection', function(arg) {
-       alert('NewDirection is called');
-     }
-   );
-   */
+		Crafty.addEvent(this, Crafty.stage.elem, "mousedown", this._onmousedown);
 
-        var norm=function(x, y) {
-          absx=Math.abs(x);
-          absy=Math.abs(y);
+		var norm = function (x, y) {
+			absx = Math.abs(x);
+			absy = Math.abs(y);
 
-          if(absx>absy) {
-            max=absx;
-            min=absy;
-          } else {
-            max=absy;
-            min=absx;
-          }
+			if (absx > absy) {
+				max = absx;
+				min = absy;
+			} else {
+				max = absy;
+				min = absx;
+			}
 
-          return max*Math.sqrt(1+Math.pow(min/max,2));
-        };
+			return max * Math.sqrt(1 + Math.pow(min / max, 2));
+		};
 
 
-        this.bind("EnterFrame", function () {
-            /*
-             document.getElementById('x').innerHTML=this.x;
-             document.getElementById('y').innerHTML=this.y;
-             if(this._target) {
-             document.getElementById('targetx').innerHTML=this._target.x;
-             document.getElementById('targety').innerHTML=this._target.y;
-           }
-           */
-        if (this.disableControls || !this._target) return;
-        if (norm(this._target.x - this.x, this._target.y - this.y) < this._speed) {
-          var prev_pos = {
-            x:this.x,
-            y:this.y
-          }
-          this._movement={
-            x:this._target.x - this.x,
-            y:this._target.y - this.y
-          };
-          this.x=this._target.x;
-          this.y=this._target.y;
-          this._target=undefined;
-          this.trigger('Moved', this.prev_pos);
-        }
+		this.bind("EnterFrame", function () {
 
-        if (this._movement.x !== 0) {
-          this.x += this._movement.x;
-          this.trigger('Moved', { x: this.x - this._movement.x, y: this.y });
-        }
-        if (this._movement.y !== 0) {
-          this.y += this._movement.y;
-          this.trigger('Moved', { x: this.x, y: this.y - this._movement.y });
-        }
-      });
-  }
+			if (this.disableControls || !this._target) {
+				return;
+			}
+
+			// target reached. stop moving, jump the last part, clear the target, trigger Moved and NewDirection
+			if (norm(this._target.x - this.x, this._target.y - this.y) < this._speed) {
+				var prev_pos = {
+					x: this.x,
+					y: this.y
+				};
+				this._movement = {
+					x: 0,
+					y: 0
+				};
+				this.x = this._target.x;
+				this.y = this._target.y;
+				this._target = undefined;
+				this.trigger('Moved', this.prev_pos);
+				this.trigger('NewDirection', this._movement);
+			};
+
+			if (this._movement.x !== 0) {
+				this.x += this._movement.x;
+				this.trigger('Moved', { x: this.x - this._movement.x, y: this.y });
+			};
+
+			if (this._movement.y !== 0) {
+				this.y += this._movement.y;
+				this.trigger('Moved', { x: this.x, y: this.y - this._movement.y });
+			}
+		});
+	}
 });
